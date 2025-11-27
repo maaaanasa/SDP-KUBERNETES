5a4957dae63f53e9c4a316f4e1c28d3b3a9204c3
manasa <2300030342@kluniversity.in>
2025-11-27 22:11:44 +0530

    updated

--- TASKMANAGER-SPRINGBOOT/src/main/java/com/taskmanager/TaskManagerApplication.java
+++ TASKMANAGER-SPRINGBOOT/src/main/java/com/taskmanager/TaskManagerApplication.java
@@ -2,16 +2,9 @@ package com.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TaskManagerApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(TaskManagerApplication.class);
-    }
+public class TaskManagerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskManagerApplication.class, args);
